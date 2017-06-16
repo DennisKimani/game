b1678fb0115cb6581b47854425be71ee8b973e16
DennisKimani <dennisngugi.dk@gmail.com>
2017-06-16 15:32:05 +0300

    added a new form in which people may enter any upcoming games

--- app/components/new-game.js
+++ app/components/new-game.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewGame: false,
+  actions: {
+    gameFormShow() {
+      this.set('addNewGame', true);
+    }
+  }
 });
